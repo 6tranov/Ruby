27320fab5d450974b441653478ac590d1ec033cf
6tranov <46306648+6tranov@users.noreply.github.com>
2019-01-16 12:12:16 +0900

    Update 1346.rb
    WheelをGearに注入するような変更ができないとき

--- 1346.rb
+++ 1346.rb
@@ -1,19 +1,18 @@
 class Gear
-    attr_reader :chainring, :cog, :wheel
-    def initialize(chainring,cog,wheel)
+    attr_reader :chainring,:cog,:rim,:tire,:wheel
+    def initialize(chainring,cog,rim,tire)
         @chainring = chainring
         @cog = cog
-        @wheel = wheel
-    end
-    
-    def ratio
-        chainring / cog.to_f
+        @wheel = Wheel.new(rim,tire)
     end
     
     def gear_inches
         ratio * wheel.diameter
     end
     
+    def ratio
+        chainring / cog.to_f
+    end
 end
 
 class Wheel
@@ -28,4 +27,4 @@ class Wheel
     end
 end
 
-Gear.new(52,11,Wheel.new(26,1.5)).gear_inches
+Gear.new(52,11,26,1.5).gear_inches
